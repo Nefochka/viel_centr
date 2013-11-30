bd5e12d0daea2cbd1b9dc83a78cb2d2cd8764346
Nefochka <nefirtity@mail.ru>
2013-11-30 20:24:15 +0400

    commit9

---
+++ db/migrate/20131124174659_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :nickname, :null => true
+      t.string :first_name, :null => true
+      t.string :last_name, :null => true
+      t.string :email, :null => false
+      t.string :crypted_password, :null => false
+      t.string :password_salt, :null => false
+      t.boolean :admin, :null => false, :default => false
+      t.string :persistence_token, :null => false
+      t.integer :login_count, :default => 0, :null => false
+      t.datetime :last_request_at
+      t.datetime :last_login_at
+      t.datetime :current_login_at
+      t.string :last_login_ip
+      t.string :current_login_ip
+      t.timestamps    
+    end
+    add_index :users, :email, :unique => true
+  end
+end
