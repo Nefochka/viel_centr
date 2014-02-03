5932e0eaab32881c8ff75f6d32ee57def8441b96
Nefochka <nefirtity@mail.ru>
2014-02-03 22:02:59 +0400

    fixed: change buttons

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
 
-  default from: 'notifications@example.com'
+  default from: 'parshukova.elena@gmail.com'
  
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to Viel-centr.ru Site')
   end
 end
