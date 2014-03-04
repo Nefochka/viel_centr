0a419144d0e0a0e324fda7464d8741211a580bbb
Nefochka <nefirtity@mail.ru>
2014-03-04 16:40:53 +0400

    fixed: change social buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def facebook_like_button_settings
+    {
+      'data-href' => "http://viel-centr.herokuapp.com/",
+      'data-width' => "90",
+      'data-height' => "20",
+      'data-colorscheme' => "light",
+      'data-layout' => "button_count",
+      'data-action' => "recommend",
+      'data-show-faces' => "false",
+      'data-send' => "false"
+    }
+  end
+
+  def twitter_button_settings
+    {
+        :href=>"https://twitter.com/share",
+        :class=>"twitter-share-button",
+        :url=>"http://viel-centr.herokuapp.com/",
+        :related=>"jasoncosta",
+        :lang=>"ru",
+        :size=>"large",
+        :count=>"none"
+    }
+  end
+
 end
---
+++ config/initializers/vkontakte.rb
@@ -0,0 +1,1 @@
+VK_APP_ID = Rails.env.production? ? 3954483 : 3954483
