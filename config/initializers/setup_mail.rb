de96f84a851da98dc40cd063ef686ee73824a191
Nefochka <nefirtity@mail.ru>
2014-03-28 15:32:28 +0400

    fixed: change mail_setup page

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
   :domain => "gmail.com",
-  :user_name => "parshukova.elena",
-  :password => "_Opel226",
+  :user_name => "vielcentr",
+  :password => "vielcentr",
   :authentication => "plain",
   :enable_starttls_auto => true
 }
